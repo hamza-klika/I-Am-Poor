8480ab678580416df2a41ee288fec597a2330b1d
Hamza Kovacevic <hamza.kovacevic@klika.ba>
2025-06-26 16:16:57 +0200

    Learn about xcode and implement static app

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Hamza Kovacevic on 17. 7. 2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
